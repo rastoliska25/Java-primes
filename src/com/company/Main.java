48d13166be6d0d81856f6ca93f479944dddc80e3
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-10 13:29:31 +0200

    prime numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,7 @@ Scanner sc = new Scanner(System.in);
         Double x = Double.parseDouble(y);
         Double q = Math.sqrt(x);
         int z=0;
+        int delitel = 0;
 
         // prvá časť - zistenie prvočísla
 for (int i=1; i<q+1;i++){
@@ -23,29 +24,50 @@ for (int i=1; i<q+1;i++){
 if (z==1){
     System.out.println("Číslo je prvočíslo. Ak chcete pokračovať na rozklad čísel, zadajte párne číslo.");
 }
-else
-    System.out.println("číslo nie je prvočíslo a počet delitelov je "+ (z+1)); //upraviť
+else {
+    for (int i =1; i<x+1;i++){
+        if (x%i==0){
+            delitel++;
+        }
+    }
+    System.out.println("číslo nie je prvočíslo a počet delitelov je " + (delitel)); //upraviť
+}
 
 // druhá časť - Goldbachova hypotéza - even number = sum of two odd numbers
 
+
         if (z!=1){
-            System.out.println("zadajte 1, ak chcete nájsť dva prvé prvočíselné súčty čísla , alebo 2, chcete vypísať všetky dvojice prvočíselných súčtov");
+            System.out.println("zadajte 1, ak chcete nájsť dve prvé prvočíselné súčty čísla , alebo 2, chcete vypísať všetky dvojice prvočíselných súčtov");
             String vstup = sc.nextLine();
             int x1 = Integer.parseInt(vstup);
             if (x1==1){
                 for (int i=1; i<x;i++){
-                   if ((i%2!=0) && ((x-i)%2!=0)){
-                       System.out.println("Prvé dve prvočísla, ktorých súčet je rovný danému číslu: "+ i +" a "+(int)((x-i)));
-                       break;
+                    int z1 = 0;
+                    int z2 = 0;
+
+                    for (int j=1; j<q+1;j++) {
+                        //if ((q==i)||(q==(x-i))){
+                        //    continue;
+                       // }
+                        if ((x-i)%j==0) {
+                            z1++;
+                        }
+                        if ((i%j)==0){
+                            z2++;
+                        }
+                        }
+                    if ((z2==1) && (z1==1)) {
+                        System.out.println("Prvočísla, ktorých súčet je rovný danému číslu: " + i + " a " + (int) ((x - i)));
+                        break;
+                        }
+                }
                     }
                 }
 
             }
         }
+
 //// next
 
 
 
-
-    }
-}
