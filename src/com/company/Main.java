a3eafeaf274a48e84d515a7b43914f8297e7b6f4
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-10 09:26:47 +0200

    prime numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,24 @@ Scanner sc = new Scanner(System.in);
         Double x = Double.parseDouble(y);
         Double q = Math.sqrt(x);
         int z=0;
-for (int i=2; i<q+1;i++){
+
+        // prvá časť - zistenie prvočísla
+for (int i=1; i<q+1;i++){
     if (x%i==0){
         z++;
-        System.out.print(i);
     }
 
 }
-System.out.println("počet delitelov je "+ z);
+
+if (z==1){
+    System.out.println("číslo je prvočíslo");
+}
+else
+    System.out.println("číslo nie je prvočíslo a počet delitelov je "+ (z+1));
+
+// druhá časť - Goldbachova hypotéza - even number = sum of two odd numbers
+
+
 
     }
 }
