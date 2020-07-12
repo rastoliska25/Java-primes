2c3addf22288896f2d0e7cc102e20445da4b06a0
rastoliska25 <67071249+rastoliska25@users.noreply.github.com>
2020-07-12 10:38:15 +0200

    prime numbers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,65 +5,75 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-        System.out.println("napíšte číslo:");
-        String y = sc.nextLine();
-        Double x = Double.parseDouble(y);
-        Double q = Math.sqrt(x);
-        int z=0;
-        int delitel = 0;
+        Scanner sc = new Scanner(System.in);
+        String pokracovat = "ano";
 
-        // prvá časť - zistenie prvočísla
-for (int i=1; i<q+1;i++){
-    if (x%i==0){
-        z++;
-    }
+        while (pokracovat.equals("ano")) {
+            System.out.println("napíšte číslo:");
+            String y = sc.nextLine();
+            Double x = Double.parseDouble(y);
+            Double q = Math.sqrt(x);
 
-}
+            int z = 0;
+            int delitel = 0;
+            // prvá časť - zistenie prvočísla
+            for (int i = 1; i < q + 1; i++) {
+                if (x % i == 0) {
+                    z++;
+                }
 
-if (z==1){
-    System.out.println("Číslo je prvočíslo. Ak chcete pokračovať na rozklad čísel, zadajte párne číslo.");
-}
-else {
-    for (int i =1; i<x+1;i++){
-        if (x%i==0){
-            delitel++;
-        }
-    }
-    System.out.println("číslo nie je prvočíslo a počet delitelov je " + (delitel)); //upraviť
-}
+            }
+
+            if (z == 1) {
+                System.out.println("Číslo je prvočíslo. Ak chcete pokračovať na rozklad čísel, zadajte párne číslo.");
+            } else {
+                for (int i = 1; i < x + 1; i++) {
+                    if (x % i == 0) {
+                        delitel++;
+                    }
+                }
+                System.out.println("číslo nie je prvočíslo a počet delitelov je " + (delitel)); //upraviť
+            }
 
 // druhá časť - Goldbachova hypotéza - even number = sum of two odd numbers
 
 
-        if (z!=1){
-            System.out.println("zadajte 1, ak chcete nájsť dve prvé prvočíselné súčty čísla , alebo 2, chcete vypísať všetky dvojice prvočíselných súčtov");
-            String vstup = sc.nextLine();
-            int x1 = Integer.parseInt(vstup);
-            if (x1==1){
-                for (int i=1; i<x;i++){
-                    int z1 = 0;
-                    int z2 = 0;
+            if (z != 1) {
+                System.out.println("zadajte 1, ak chcete nájsť dve prvé prvočíselné súčty čísla , alebo 2, chcete vypísať všetky dvojice prvočíselných súčtov");
+                String vstup = sc.nextLine();
+                int x1 = Integer.parseInt(vstup);
+                if (x1 == 1) {
+                    for (int i = 1; i < x; i++) {
+                        int z1 = 0;
+                        int z2 = 0;
 
-                    for (int j=2; j<q+1;j++) {
+                        for (int j = 2; j < q + 1; j++) {
 
-                        if ((x-i)%j==0) {
-                            z1++;
+                            if ((x - i) % j == 0) {
+                                z1++;
+                            }
+                            if ((i % j) == 0) {
+                                z2++;
+                            }
                         }
-                        if ((i%j)==0){
-                            z2++;
+                        if ((z2 == 1) && (z1 == 1)) {
+                            System.out.println("Prvočísla, ktorých súčet je rovný danému číslu: " + i + " a " + (int) ((x - i)));
+                            break;
                         }
-                        }
-                    if ((z2==1) && (z1==1)) {
-                        System.out.println("Prvočísla, ktorých súčet je rovný danému číslu: " + i + " a " + (int) ((x - i)));
-                        break;
-                        }
-                }
                     }
                 }
-        System.out.println("github test");
             }
+            System.out.println("chcete zadat dalsie cislo? (ano/nie)");
+                    pokracovat = sc.nextLine();
+
+
+
+
+
         }
+        System.out.println("Ďakujem za použitie programu prvočísla");
+    }
+}
 
         //primes
 
